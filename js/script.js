67c139e759e159a03cd24322985ffed925fbf797
spacecreatedd <kimzhenya1313@mail.ru>
2024-01-23 11:59:42 +0700

    fifth

--- js/script.js
+++ js/script.js
@@ -15,53 +15,76 @@ let app = new Vue({
     inputFor: null,
     inputFiv: null,
   },
-  template: `
-  <div id="app">
+  template: ` <div id="app">
   <form class="addCardForm" @submit.prevent="addCard">
-  <p class="form-p">
-    <label for="GroupName">Название карточки:</label>
-    <input id="GroupName" v-model="groupName">
-  </p>
-  <p class="form-p">
-    <label for="InputOne">Пункт 1:</label>
-    <input id="InputOne" v-model="inputOne">
-  </p>
-  <p class="form-p">
-    <label for="InputTwo">Пункт 2:</label>
-    <input id="InputTwo" v-model="inputTwo">
-  </p>
-  <p class="form-p">
-    <label for="InputThr">Пункт 3:</label>
-    <input id="InputThr" v-model="inputThr">
-  </p>
-  <p class="form-p">
-    <label for="InputFor">Пункт 4:</label>
-    <input id="InputFor" v-model="inputFor">
-  </p>
-  <p class="form-p">
-    <label for="InputFiv">Пункт 5:</label>
-    <input id="InputFiv" v-model="inputFiv">
-  </p>
-  <p class="form-p button">
-    <input type="submit" value="Создать">
-  </p>
-</form>
-    <div class="columns" style="display: flex; justify-content: space-evenly;">
-      <div class="column">
-        <h2>Первый столбец</h2>
-        </div>
+    <p class="form-p">
+      <label for="GroupName">Название карточки:</label>
+      <input id="GroupName" v-model="groupName">
+    </p>
+    <p class="form-p">
+      <label for="InputOne">Пункт 1:</label>
+      <input id="InputOne" v-model="inputOne">
+    </p>
+    <p class="form-p">
+      <label for="InputTwo">Пункт 2:</label>
+      <input id="InputTwo" v-model="inputTwo">
+    </p>
+    <p class="form-p">
+      <label for="InputThr">Пункт 3:</label>
+      <input id="InputThr" v-model="inputThr">
+    </p>
+    <p class="form-p">
+      <label for="InputFor">Пункт 4:</label>
+      <input id="InputFor" v-model="inputFor">
+    </p>
+    <p class="form-p">
+      <label for="InputFiv">Пункт 5:</label>
+      <input id="InputFiv" v-model="inputFiv">
+    </p>
+    <p class="form-p button">
+      <input type="submit" value="Создать">
+    </p>
+  </form>
+  <div class="columns" style="display: flex; justify-content: space-evenly;">
+    <div class="column">
+      <h2>Первый столбец</h2>
+      <div class="card" v-for="(group, groupIndex) in firstColumn" :key="group.id">
+        <h3>{{ group.groupName }}</h3>
+        <ul>
+          <li v-for="(item , itemIndex) in group.items" :key="item.id">
+            <input type="checkbox" v-model="item.checked"  @change="updateProgress(group)">
+            {{ item.text }}
+          </li>
+        </ul>
       </div>
-      <div class="column">
-        <h2>Второй столбец</h2>
-        <div class="card" v-for="(group, groupIndex) in secondColumn" :key="group.id">
-        </div>
+    </div>
+    <div class="column">
+      <h2>Второй столбец</h2>
+      <div class="card" v-for="(group, groupIndex) in secondColumn" :key="group.id">
+        <h3>{{ group.groupName }}</h3>
+        <ul>
+          <li v-for="(item , itemIndex) in group.items" :key="item.id">
+            <input type="checkbox"  v-model="item.checked" @change="updateProgress(group)">
+            {{ item.text }}
+          </li>
+        </ul>
       </div>
-      <div class="column">
-        <h2>Третий столбец</h2>
+    </div>
+    <div class="column">
+      <h2>Третий столбец</h2>
+      <div class="card" v-for="group in thirdColumn" :key="group.id">
+        <h3>{{ group.groupName }}</h3>
+        <ul>
+          <li v-for="item in group.items" :key="item.id">
+            <input type="checkbox"  v-model="item.checked">
+            {{ item.text }}
+          </li>
+        </ul>
+        <p v-if="group.isComplete"> {{ group.lastChecked }}</p>
       </div>
     </div>
   </div>
-  `,
+</div>`,
   watch: {
     firstColumn: {
       handler(newFirstColumn) {
@@ -117,7 +140,8 @@ let app = new Vue({
           card.lastChecked = new Date().toLocaleString();
         }
         this.checkMoveCard();
-      }, MoveFirst() {
+      }, 
+      MoveFirst() {
         if (this.secondColumn.length >= 5) {
           const isCardOver50Percent = this.secondColumn.some(card => {
             const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
@@ -132,6 +156,31 @@ let app = new Vue({
             return;
           }
         }
+      },
+      MoveSecond() {
+        this.secondColumn.forEach(card => {
+          const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
+          if (progress === 100) {
+            card.isComplete = true;
+            card.lastChecked = new Date().toLocaleString();
+            this.thirdColumn.push(card);
+            this.secondColumn.splice(this.secondColumn.indexOf(card), 1);
+            this.MoveFirst();
+          }
+        });
+        this.isFirstColumnDisabled = this.secondColumn.length >= 5;
+      },
+      saveData() {
+        const data = {
+          firstColumn: this.firstColumn,
+          secondColumn: this.secondColumn,
+          thirdColumn: this.thirdColumn
+        };
+        localStorage.setItem(storageKey, JSON.stringify(data));
+      },
+      checkMoveCard() {
+        this.MoveFirst();
+        this.MoveSecond();
       },
   }
 })
